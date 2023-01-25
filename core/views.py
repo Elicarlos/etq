c28c36225f6b07696be710ce3ad34634f388d939
Elicarlos <elicarlosantos_@hotmail.com>
2023-01-25 06:52:10 -0300

    ajueste

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from . models import Produto, Tipo, Empresa
 
 # Create your views here.
+
+def gerar_etiqueta(request, id):
+    produto = Produto.objects.get(id=id)
+    print(produto.descricao)
+    
+
+
+gerar_etiqueta()
